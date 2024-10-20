7571216591fb810264d82eeba173c2f3c46cdd92
Maïwenn Plevenage <plevenagemaiwenn@gmail.com>
2024-10-20 18:24:06 +0200

    Boucle pour le script comptes.sh et mise à jour du journal avec explication du code de la diapo

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,36 +1,20 @@
 #!/usr/bin/bash
 
-ANNEE1=2016
-ANNEE2=2017
-ANNEE3=2018
-
-#test qui check si chacun des dossiers existe
-if [ -d ~/Exercice1/ann/$ANNEE1 ] && [ -d ~/Exercice1/ann/$ANNEE2 ] && [ -d ~/Exercice1/ann/$ANNEE3 ]
+#test qui check que le dossier existe
+if [ -d ~/Exercice1/ann/ ]
 
 then
 
+cd ~/Exercice1/ann/
+
+for dir in */
+do
+echo "Annotations de lieux en ${dir%/}"
 #boucle qui check si les fichiers existent dans le dossier
-for i in  ~/Exercice1/ann/$ANNEE1/*/*.ann
+for i in  ~/Exercice1/ann/"$dir"/*/*.ann
 do test -f "$i"
 done
-echo "Annotations de lieux en $ANNEE1"
-cd ~/Exercice1/ann/$ANNEE1
-cat */*.ann | egrep "Location" | wc
-cd ../
-cd $ANNEE2
-
-for i in ~/Exercice1/ann/$ANNEE2/*/*.ann
-do test -f "$i"
+cat $dir/*/*.ann | egrep "Location" | wc
 done
-echo "Annotations de lieux en $ANNEE2"
-cat */*.ann | egrep "Location" | wc
-cd ../
-cd $ANNEE3
-
-for i in ~/Exercice1/ann/$ANNEE3/*/*.ann
-do test -f "$i"
-done
-echo "Annotations de lieux en $ANNEE3"
-cat */*.ann | egrep "Location" | wc
 
 fi
