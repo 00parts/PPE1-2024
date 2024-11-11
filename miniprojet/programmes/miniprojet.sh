0e42365b60444a361e9b6db74f0707ee1404455a
Maïwenn Plevenage <plevenagemaiwenn@gmail.com>
2024-11-11 16:53:45 +0100

    Exercice 2 (Modifications du script + création du tableau en output du script)

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -17,8 +17,13 @@ fi
 i=1
 while read -r line
 do
-echo -e "$i \t $line"
+code=$(curl -ILs "$line" | grep "HTTP/" | cut -d' ' -f2)
+charset=$(curl -ILs "$line" | grep -i "Content-Type:" | cut -d" " -f3 | cut -d"=" -f2 | sed "s/\r$//")
+if [ -z "$charset" ]
+then
+charset="no_info"
+fi
+count=$(lynx "$line" -dump -nolist | wc -w)
+echo -e " $i \t $line \t $code \t $charset \t $count " >> ./tableaux/tableau-fr.tsv
 i=$((i + 1))
 done < $urls
-
-
